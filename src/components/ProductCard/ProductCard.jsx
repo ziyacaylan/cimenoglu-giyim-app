ac03762617db2d600d3d5e29487096d1d58e4bba
ziyacaylan <ziyacaylan@gmail.com>
2023-01-13 11:30:15 +0300

    almost complete

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,51 +3,73 @@ import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import "./ProductCard.styles.scss";
 
 const ProductCard = ({ filteredProducts }) => {
   return (
     <>
       {filteredProducts.map((product) => (
-        <Card
-          key={product.id}
-          sx={{
-            maxWidth: 300,
-            minWidth: 250,
-            margin: "1rem",
-            ":hover": {
-              boxShadow: 20, // theme.shadows[20]
-            },
-          }}
-        >
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="300"
-              sx={{ width: "250px" }}
-              image={product.imageUrl}
-              alt="green iguana"
-            />
-
-            <CardContent>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "spaceBetween",
-                }}
-              >
-                <Typography gutterBottom variant="caption" component="span">
-                  {product.name}
-                </Typography>
-                <Box sx={{ flexGrow: 1 }}></Box>
-                <Typography variant="h6" component="div">
-                  {`${product.price}$`}
-                </Typography>
+        <Box key={product.id}>
+          <Card
+            className="collection-item"
+            sx={{
+              maxWidth: 300,
+              minWidth: 250,
+              margin: "1rem",
+              zIndex: "1",
+              ":hover": {
+                boxShadow: 20,
+              },
+            }}
+            style={{ position: "relative", width: "100%" }}
+          >
+            <CardActionArea>
+              <Box>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  sx={{ width: "300px" }}
+                  image={product.imageUrl}
+                  alt="green iguana"
+                />
               </Box>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+              <Button
+                className="custom-button"
+                component={Link}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  color: "primary.main",
+                }}
+                variant="contained"
+                startIcon={<ShoppingCartIcon />}
+              >
+                Add to card
+              </Button>
+              <CardContent>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "spaceBetween",
+                  }}
+                >
+                  <Typography gutterBottom variant="caption" component="span">
+                    {product.name}
+                  </Typography>
+                  <Box sx={{ flexGrow: 1 }}></Box>
+                  <Typography variant="h6" component="div">
+                    {`${product.price}$`}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Box>
       ))}
     </>
   );
--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,4 +1,3 @@
-import { Container } from "@mui/system";
 import { useSelector } from "react-redux";
 import CategoryPreview from "../../components/CategoryPreview";
 
@@ -6,11 +5,11 @@ const Shop = () => {
   const { categories } = useSelector((state) => state.categories);
 
   return (
-    <Container maxWidth="xl">
+    <>
       {categories.map((category) => (
         <CategoryPreview category={category.name} key={category.id} />
       ))}
-    </Container>
+    </>
   );
 };
 
